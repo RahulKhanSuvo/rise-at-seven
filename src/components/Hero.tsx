5d240eeec87d725fe3270b428fcd35aec9e5a809
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-08 09:50:22 +0600

    feat: replace static hero component with a randomized background image selector

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,29 @@
+"use client";
+import Image from "next/image";
+import heroImage1 from "@/assets/background/RedBull.webp";
+import heroImage2 from "@/assets/background/Pooky-Rechargable.webp";
+import { useState } from "react";
+const images = [heroImage1, heroImage2];
 export default function Hero() {
-  return <div>Hero</div>;
+  const [bg] = useState(() => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+  });
+  return (
+    <section className="relative h-screen w-full overflow-hidden">
+      <Image
+        src={bg}
+        alt="Hero background"
+        fill
+        priority
+        className="object-cover"
+      />
+
+      <div className="absolute inset-0 bg-black/40" />
+
+      <div className="relative z-10 flex h-full items-center justify-center text-white">
+        <h1 className="text-5xl font-bold">Hero Section</h1>
+      </div>
+    </section>
+  );
 }
