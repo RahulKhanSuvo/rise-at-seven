678fda35ad5a26ccc768016dd4c9194bc581bdaa
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-08 16:35:18 +0600

    refactor: wrap agency marquee component in a section element

--- src/components/agencyLogoMrq/Agencies.tsx
+++ src/components/agencyLogoMrq/Agencies.tsx
@@ -2,11 +2,20 @@ import SwiperMarquee from "./SwiperMarquee";
 
 export default function Agencies() {
   return (
-    <div className="w-full flex items-center px-4 md:px-6 bg-white">
-      <p className="  text-nowrap w-40 text-sm font-medium">
-        The agency behind...
-      </p>
-      <SwiperMarquee />
-    </div>
+    <section>
+      <div className="w-full flex flex-col md:flex-row  md:items-center px-4 md:px-6 bg-white">
+        <p className="  text-nowrap w-40 text-sm font-medium">
+          The agency behind...
+        </p>
+        <SwiperMarquee />
+      </div>
+      <div>
+        <p>
+          A global team of search-first content marketers engineering semantic
+          relevancy & category signals for both the internet and people
+        </p>
+        <div></div>
+      </div>
+    </section>
   );
 }
