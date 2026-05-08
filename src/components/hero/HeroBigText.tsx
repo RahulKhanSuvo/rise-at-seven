1c1d046f8c49d5bd5b9931a43cbd42a97153cb83
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-08 11:59:23 +0600

    refactor: update HeroBigText content and restructure layout to include additional text animation

--- src/components/hero/HeroBigText.tsx
+++ src/components/hero/HeroBigText.tsx
@@ -8,21 +8,25 @@ export default function HeroBigText({
 }) {
   const text1 = "We";
   const text2 = "Create";
-  const text3 = "What Matters";
+  const text3 = "Category";
   return (
     <div className="flex w-screen justify-center flex-col items-center ">
       <div className="flex gap-2.5">
         <TextSideAnimation text={text1} />
         <TextSideAnimation text={text2} />
       </div>
-      <TextSideAnimation text={text3} />
-      <div className="size-[113px] relative">
-        <Image
-          src={heroLogo}
-          alt="background"
-          fill
-          className="object-cover rounded-"
-        />
+      <div className="flex items-center gap-4">
+        {" "}
+        <TextSideAnimation text={text3} />
+        <div className="size-[113px] relative">
+          <Image
+            src={heroLogo}
+            alt="background"
+            fill
+            className="object-cover rounded-[17px]"
+          />
+        </div>
+        <TextSideAnimation text="Leaders" />
       </div>
     </div>
   );
