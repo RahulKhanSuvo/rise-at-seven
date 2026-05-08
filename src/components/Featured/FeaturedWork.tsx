9f10e0be243844e7d9a2018fede5a517bee6131d
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-08 22:49:49 +0600

    feat: render dynamic project list and implement secondary scroll parallax in FeaturedWork component

--- src/components/Featured/FeaturedWork.tsx
+++ src/components/Featured/FeaturedWork.tsx
@@ -2,7 +2,7 @@
 import { motion, useScroll, useTransform } from "motion/react";
 import { useRef } from "react";
 import Projects from "./Projects";
-
+import { projectsData } from "@/data/projectData";
 export default function FeaturedWork() {
   const containerRef = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -11,17 +11,25 @@ export default function FeaturedWork() {
   });
 
   const y = useTransform(scrollYProgress, [0, 1], ["0%", "-88.60%"]);
+  const y2 = useTransform(scrollYProgress, [0, 1], ["25%", "-40%"]);
 
   return (
     <section ref={containerRef} className="py-16 h-[1000vh] px-4 md:px-7">
       <div className="px-4 md:px-6 flex h-[90vh] overflow-hidden justify-between items-center flex-wrap sticky top-10 bg-black text-white rounded-3xl">
         <div className="flex flex-col gap-y-4">
           <p className="text-sm uppercase tracking-widest opacity-60">
-            Selected Projects
+            Featured Work
           </p>
-          <h2 className="text-4xl lg:text-6xl xl:text-7xl font-medium tracking-tight leading-[0.9] uppercase">
-            Featured <br /> Work
-          </h2>
+          <motion.div style={{ y: y2 }} className="flex flex-col gap-y-4">
+            {projectsData.map((project) => (
+              <div key={project.id}>
+                <h2 className="text-4xl lg:text-6xl xl:text-7xl font-medium tracking-tight leading-[0.9] uppercase">
+                  {project.title}
+                </h2>
+                <span>{project.timeline}</span>
+              </div>
+            ))}
+          </motion.div>
         </div>
         <div className="w-3/7 h-full items-center overflow-hidden">
           <motion.div style={{ y }}>
