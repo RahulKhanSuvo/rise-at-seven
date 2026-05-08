254341607e616cd64fe776ae56bd921b4cd18062
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-09 01:28:12 +0600

    feat: add View All Projects button to FeaturedWork component

--- src/components/Featured/FeaturedWork.tsx
+++ src/components/Featured/FeaturedWork.tsx
@@ -4,6 +4,7 @@ import { useRef, useState } from "react";
 import Link from "next/link";
 import Projects from "./Projects";
 import { projectsData } from "@/data/projectData";
+import Button from "@/utils/Button";
 
 export default function FeaturedWork() {
   const containerRef = useRef(null);
@@ -18,56 +19,61 @@ export default function FeaturedWork() {
   const y2 = useTransform(scrollYProgress, [0, 1], ["25%", "-50%"]);
 
   return (
-    <section ref={containerRef} className="py-16 h-[1000vh] px-4 md:px-7">
-      <div className="px-4 md:px-10 flex h-[90vh] overflow-hidden justify-between items-center flex-wrap sticky top-20 bg-black text-white rounded-3xl">
-        <div className="flex flex-col relative">
-          <div className="absolute inset-0 z-20 flex justify-between flex-col pointer-events-none">
-            <div>
-              <p className="text-lg md:text-2xl font-semibold bg-black w-full pt-16 pb-10">
-                Featured Work
-              </p>
-              <div className="w-full h-32 pointer-events-none bg-linear-to-b from-black to-transparent "></div>
+    <>
+      <section ref={containerRef} className="pt-16 h-[1000vh] px-4 md:px-7">
+        <div className="px-4 md:px-10 flex h-[90vh] overflow-hidden justify-between items-center flex-wrap sticky top-20 bg-black text-white rounded-3xl">
+          <div className="flex flex-col relative">
+            <div className="absolute inset-0 z-20 flex justify-between flex-col pointer-events-none">
+              <div>
+                <p className="text-lg md:text-2xl font-semibold bg-black w-full pt-16 pb-10">
+                  Featured Work
+                </p>
+                <div className="w-full h-32 pointer-events-none bg-linear-to-b from-black to-transparent "></div>
+              </div>
+              <div>
+                <div className="w-full h-50 pointer-events-none bg-linear-to-b from-black/5 to-black "></div>
+                <div className="text-lg md:text-2xl font-semibold bg-black w-full h-60 "></div>
+              </div>
             </div>
-            <div>
-              <div className="w-full h-50 pointer-events-none bg-linear-to-b from-black/5 to-black "></div>
-              <div className="text-lg md:text-2xl font-semibold bg-black w-full h-60 "></div>
-            </div>
-          </div>
-          <motion.div style={{ y: y2 }} className="flex flex-col gap-y-4">
-            {projectsData.map((project) => (
-              <Link
-                href={`/projects/${project.id}`}
-                key={project.id}
-                onMouseEnter={() => setHoveredProjectId(project.id)}
-                onMouseLeave={() => setHoveredProjectId(null)}
-              >
-                <motion.div
-                  animate={{
-                    x: hoveredProjectId === project.id ? 20 : 0,
-                  }}
-                  transition={{ type: "spring", stiffness: 400, damping: 25 }}
-                  className="flex items-start gap-x-2 z-30 relative cursor-pointer"
+            <motion.div style={{ y: y2 }} className="flex flex-col gap-y-4">
+              {projectsData.map((project) => (
+                <Link
+                  href={`/projects/${project.id}`}
+                  key={project.id}
+                  onMouseEnter={() => setHoveredProjectId(project.id)}
+                  onMouseLeave={() => setHoveredProjectId(null)}
                 >
-                  <h2 className="text-4xl lg:text-6xl xl:text-[4.8rem] font-semibold tracking-tight leading-[0.9]">
-                    {project.title}
-                  </h2>
-                  <span className="text-white text-xs font-medium mt-2">
-                    {`[${project.timeline}]`}
-                  </span>
-                </motion.div>
-              </Link>
-            ))}
-          </motion.div>
+                  <motion.div
+                    animate={{
+                      x: hoveredProjectId === project.id ? 20 : 0,
+                    }}
+                    transition={{ type: "spring", stiffness: 400, damping: 25 }}
+                    className="flex items-start gap-x-2 z-30 relative cursor-pointer"
+                  >
+                    <h2 className="text-4xl lg:text-6xl xl:text-[4.8rem] font-semibold tracking-tight leading-[0.9]">
+                      {project.title}
+                    </h2>
+                    <span className="text-white text-xs font-medium mt-2">
+                      {`[${project.timeline}]`}
+                    </span>
+                  </motion.div>
+                </Link>
+              ))}
+            </motion.div>
+          </div>
+          <div className="w-3/7 h-full items-center">
+            <motion.div style={{ y }}>
+              <Projects
+                hoveredProjectId={hoveredProjectId}
+                setHoveredProjectId={setHoveredProjectId}
+              />
+            </motion.div>
+          </div>
         </div>
-        <div className="w-3/7 h-full items-center">
-          <motion.div style={{ y }}>
-            <Projects
-              hoveredProjectId={hoveredProjectId}
-              setHoveredProjectId={setHoveredProjectId}
-            />
-          </motion.div>
-        </div>
+      </section>
+      <div className="text-center pt-8">
+        <Button text="View All Projects" href="#" variant="solid" />
       </div>
-    </section>
+    </>
   );
 }
