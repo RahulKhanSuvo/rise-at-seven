cc2bc656d064cd5bc34407188b7d0cf68ef9b876
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-09 06:24:18 +0600

    feat: add layout padding and call-to-action button to OurServices component

--- src/components/OurServices/OurServices.tsx
+++ src/components/OurServices/OurServices.tsx
@@ -1,21 +1,25 @@
 import AnimatedLogo from "@/common/AnimatedLogo";
 import TextSideAnimation from "@/common/TextSideAnimation";
 import heroLogo from "@/assets/banner/IMG_5079.webp";
+import Button from "@/common/Button";
 
 export default function OurServices() {
   return (
-    <section>
+    <section className="py-10 px-4 md:px-7">
       {/* tile */}
-      <div className="flex flex-wrap gap-2.5">
-        <TextSideAnimation
-          className="text-5xl md:text-7xl lg:text-8xl xl:text-[7.4rem]"
-          text="Our"
-        />
-        <AnimatedLogo src={heroLogo} delay={0.8} />
-        <TextSideAnimation
-          className="text-5xl md:text-7xl lg:text-8xl xl:text-[7.4rem]"
-          text="Services"
-        />
+      <div className="flex justify-between items-center">
+        <div className="flex flex-wrap gap-2.5">
+          <TextSideAnimation
+            className="text-5xl md:text-7xl lg:text-8xl xl:text-[7.4rem]"
+            text="Our"
+          />
+          <AnimatedLogo src={heroLogo} delay={0.8} />
+          <TextSideAnimation
+            className="text-5xl md:text-7xl lg:text-8xl xl:text-[7.4rem]"
+            text="Services"
+          />
+        </div>
+        <Button href="#" text="Visit All Services" />
       </div>
     </section>
   );
