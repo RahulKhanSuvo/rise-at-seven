02c4a6ea61a452cf36c46dd17eaa98c8e312a50f
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-09 07:57:13 +0600

    feat: update BriefMarquee images and adjust responsive typography and layout styling

--- src/components/BriefMarquee/BriefMarquee.tsx
+++ src/components/BriefMarquee/BriefMarquee.tsx
@@ -2,8 +2,8 @@
 import { motion } from "motion/react";
 import Image from "next/image";
 import Link from "next/link";
-import img1 from "@/assets/background/Screenshot-2025-07-01-at-21.36.35.webp";
-import img2 from "@/assets/background/unnamed-6.webp";
+import img1 from "@/assets/banner/lovely.png";
+import img2 from "@/assets/banner/gangs.png";
 import BriefCursor from "./BriefCursor";
 
 export default function BriefMarquee() {
@@ -45,12 +45,12 @@ export default function BriefMarquee() {
           {displayItems.map((item, idx) => (
             <div
               key={idx}
-              className="flex items-center gap-x-10 lg:gap-x-20 shrink-0"
+              className="flex items-center gap-x-10 lg:gap-x-15 shrink-0"
             >
-              <h2 className="text-7xl md:text-8xl lg:text-9xl xl:text-[10rem] font-medium tracking-tight text-grey-900 leading-[0.9]">
+              <h2 className="text-7xl md:text-8xl lg:text-9xl xl:text-[11rem] font-semibold tracking-tight text-grey-900 leading-[0.9]">
                 {item.text}
               </h2>
-              <div className="w-[25vw] md:w-[20vw] lg:w-[15vw] xl:w-[12vw] aspect-square rounded-2xl lg:rounded-3xl overflow-hidden relative shrink-0">
+              <div className="w-[25vw] md:w-[20vw] lg:w-[15vw] xl:w-[10vw] aspect-square rounded-2xl lg:rounded-3xl overflow-hidden relative shrink-0">
                 <Image
                   src={item.image}
                   alt={item.text}
