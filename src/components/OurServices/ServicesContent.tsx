0d15fd9d5d308ad788fdac89852a887a4825c1e8
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-09 06:41:35 +0600

    feat: implement services content mapping and adjust indentation in OurServices component

--- src/components/OurServices/ServicesContent.tsx
+++ src/components/OurServices/ServicesContent.tsx
@@ -1,3 +1,15 @@
+import { servicesContentData } from "@/data/servicesContentData";
+import Image from "next/image";
+
 export default function ServicesContent() {
-  return <div></div>;
+  return (
+    <div>
+      {servicesContentData.map((item) => (
+        <div key={item.id}>
+          <Image src={item.image} alt={item.title} />
+          <h3>{item.title}</h3>
+        </div>
+      ))}
+    </div>
+  );
 }
