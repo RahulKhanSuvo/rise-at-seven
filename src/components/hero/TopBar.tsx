985c0a9dca6d8536ab40b337bb52d922d581ff7a
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-09 07:18:24 +0600

    feat: update top bar button text and style for category leaderboard announcement

--- src/components/hero/TopBar.tsx
+++ src/components/hero/TopBar.tsx
@@ -4,9 +4,10 @@ export default function TopBar() {
   return (
     <div className=" w-full  rounded-3xl p-2">
       <Button
-        text="#1 Most recommended content marketing agency"
+        text="🚨 The Category Leaderboard - Live Now"
         href="#"
         variant="solid"
+        className="bg-primary w-full py-1 border-none"
       />
     </div>
   );
