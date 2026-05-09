05c269b49fb862056ba1faaed34e93f43a016b18
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-09 19:16:46 +0600

    refactor: wrap footer content in an additional container div for improved layout structure

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,12 +7,14 @@ export default function Footer() {
   return (
     <footer className=" text-white pt-20 px-4 pb-2 md:px-2 w-full overflow-hidden flex flex-col items-center">
       <div className="w-full bg-[#111212] rounded-3xl px-6.5 py-8">
-        <div className="flex flex-col md:flex-row justify-between items-start gap-16 md:gap-0">
-          <NewsletterSection />
-          <FooterLinks />
+        <div>
+          <div className="flex flex-col md:flex-row justify-between items-start gap-16 md:gap-0">
+            <NewsletterSection />
+            <FooterLinks />
+          </div>
+          <FooterLogo />
+          <FooterBottomBar />
         </div>
-        <FooterLogo />
-        <FooterBottomBar />
       </div>
     </footer>
   );
