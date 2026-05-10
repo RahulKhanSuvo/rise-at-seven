ed386fb749bc2a7b59cfdbfb94ed531cd5eb8655
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-10 08:39:47 +0600

    refactor: update layout responsiveness, adjust logo size, and reformat button components in Agencies marquee section

--- src/components/agencyLogoMrq/Agencies.tsx
+++ src/components/agencyLogoMrq/Agencies.tsx
@@ -13,7 +13,7 @@ export default function Agencies() {
         <SwiperMarquee />
       </div>
       <div className=" py-12 md:py-24 px-4 md:px-7 flex justify-between">
-        <div className="w-full mb-1 md:mt-2 md:mb-0 max-w-sm xl:max-w-xl 3xl:max-w-2xl 4xl:max-w-3xl">
+        <div className="w-full mb-1 md:mt-2 hidden md:block md:mb-0 max-w-sm xl:max-w-xl 3xl:max-w-2xl 4xl:max-w-3xl">
           <p className="md:inline-flex flex-wrap hidden text-balance relative text-left justify-start text-black text-lg/tight lg:text-lg/tight xl:text-2xl 4xl:text-3xl font-medium xl:leading-[1.3] tracking-tight">
             A global team of search-first content marketers engineering semantic
             relevancy & category signals for both the internet and people
@@ -21,13 +21,13 @@ export default function Agencies() {
         </div>
 
         <div className="flex flex-col ">
-          <div className="flex-wrap relative md:max-w-2xl  text-left gap-2.5 text-justify-start inline-flex  text-grey-900 text-5xl  lg:text-6xl xl:text-[5rem] 3xl:text-7.5xl 3xl font-sans-primary font-semibold tracking-tight">
+          <div className="flex-wrap relative w-full md:max-w-2xl  text-left gap-2.5 text-justify-start inline-flex  text-grey-900 text-5xl  lg:text-6xl xl:text-[5rem] 3xl:text-7.5xl 3xl font-sans-primary font-semibold tracking-tight">
             <div className="inline mr-2 pointer-fine:mr-0">Driving</div>
             <div className="inline mr-2 pointer-fine:mr-0">Demand</div>
             <div className="inline mr-2 pointer-fine:mr-0">&amp;</div>
             <div className="inline mr-2 pointer-fine:mr-0">Discovery</div>
             <div className="inline shrink-0  relative overflow-hidden mr-2 pointer-fine:mr-0">
-              <div className="size-20 relative ">
+              <div className="size-16 md:size-20 relative ">
                 <Image
                   src={banner}
                   alt=""
