db4679fbe18772ab42403ca349c18df96dcb2ee2
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-05-11 07:17:48 +0600

    refactor: replace motion-based image component with AnimatedLogo and refine responsive sizing logic

--- src/components/hero/HeroBigText.tsx
+++ src/components/hero/HeroBigText.tsx
@@ -1,9 +1,9 @@
 "use client";
-import Image, { StaticImageData } from "next/image";
+import { StaticImageData } from "next/image";
 import TextSideAnimation from "../../common/TextSideAnimation";
-import { motion } from "motion/react";
 import { useState, useCallback } from "react";
 import { useCustomResize } from "@/hook/useCustomResize";
+import AnimatedLogo from "@/common/AnimatedLogo";
 
 export default function HeroBigText({
   heroLogo,
@@ -14,7 +14,16 @@ export default function HeroBigText({
 
   useCustomResize(
     useCallback(() => {
-      setTargetWidth(window.innerWidth >= 768 ? 112 : 50);
+      if (window.innerWidth >= 1024) {
+        // desktop
+        setTargetWidth(112);
+      } else if (window.innerWidth >= 768) {
+        // tablet
+        setTargetWidth(80);
+      } else {
+        // mobile
+        setTargetWidth(55);
+      }
     }, []),
   );
 
@@ -26,23 +35,7 @@ export default function HeroBigText({
       </div>
       <div className="flex flex-wrap justify-center gap-1 md:gap-4 overflow-hidden h-fit pb-1 md:pb-6">
         <TextSideAnimation paddingBottom="pb-1 md:pb-2" text={"Category"} />
-        <motion.div
-          whileInView={{ width: targetWidth, opacity: 1 }}
-          viewport={{ once: true, amount: 0.5 }}
-          initial={{ width: 0, opacity: 0 }}
-          transition={{ duration: 1, ease: "easeInOut", delay: 0.6 }}
-          className="h-14 md:h-28 relative overflow-hidden flex justify-center rounded-xl md:rounded-3xl"
-        >
-          <div className="size-14 md:size-28 relative shrink-0">
-            <Image
-              src={heroLogo}
-              alt="background"
-              fill
-              sizes="(max-width: 768px) 44px, 112px"
-              className="object-cover"
-            />
-          </div>
-        </motion.div>
+        <AnimatedLogo src={heroLogo} size={targetWidth} />
         <TextSideAnimation text="Leaders" />
       </div>
     </div>
